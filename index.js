7d9c2e76ef955b5d2b4276b02334d1ec552721ac
Allison <allimacq@gmail.com>
2021-08-07 22:13:30 -0400

    commit tree e7dbc4926ca9d3d09ddec54e90874ece7173ce36

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    window.customerName = window.customerName.toUpperCase();
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'init';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'changed';
+}
